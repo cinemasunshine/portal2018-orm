5f3571116bbb07eaf347d27e60d40b9f5f578893
Atsushi Okui <okui@motionpicture.jp>
2020-06-19 17:51:04 +0900

    Add TheaterOpeningHour entity

---
+++ src/Entity/TheaterOpeningHour.php
@@ -0,0 +1,190 @@
+<?php
+
+/**
+ * TheaterOpeningHour.php
+ *
+ * @author Atsushi Okui <okui@motionpicture.jp>
+ */
+
+declare(strict_types=1);
+
+namespace Cinemasunshine\ORM\Entity;
+
+/**
+ * TheaterOpeningHour entity
+ *
+ * 現時点ではDoctrineのEntityとはしない。
+ * DBへはjsonデータへ変換して文字列カラムへセットする。
+ */
+class TheaterOpeningHour
+{
+    public const TYPE_DATE = 1;
+    public const TYPE_TERM = 2;
+
+    /**
+     * @var int
+     */
+    protected $type;
+
+    /**
+     * @var \DateTime
+     */
+    protected $fromDate;
+
+    /**
+     * @var \DateTime|null
+     */
+    protected $toDate;
+
+    /**
+     * @var \DateTime
+     */
+    protected $time;
+
+    /**
+     * Create this entity
+     *
+     * @param array{type:int,from_date:string,to_date:string|null,time:string} $data
+     * @return self
+     */
+    public static function create(array $data): self
+    {
+        $entity = new self();
+        $entity->setType((int) $data['type']);
+        $entity->setFromDate($data['from_date']);
+        $entity->setToDate($data['to_date']);
+        $entity->setTime($data['time']);
+
+        return $entity;
+    }
+
+    /**
+     * constructor
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * Return type
+     *
+     * @return integer
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     * @return void
+     */
+    public function setType(int $type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Return fromDate
+     *
+     * @return \DateTime
+     */
+    public function getFromDate(): \DateTime
+    {
+        return $this->fromDate;
+    }
+
+    /**
+     * Set fromDate
+     *
+     * @param \DateTime|string $fromDate
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    public function setFromDate($fromDate)
+    {
+        if ($fromDate instanceof \DateTime) {
+            $this->fromDate = $fromDate;
+        } elseif (is_string($fromDate)) {
+            $this->fromDate = new \DateTime($fromDate);
+        } else {
+            throw new \InvalidArgumentException('Invalid type.');
+        }
+    }
+
+    /**
+     * Return toDate
+     *
+     * @return \DateTime|null
+     */
+    public function getToDate(): ?\DateTime
+    {
+        return $this->toDate;
+    }
+
+    /**
+     * Set toDate
+     *
+     * @param \DateTime|string|null $toDate
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    public function setToDate($toDate)
+    {
+        if (is_null($toDate) || $toDate instanceof \DateTime) {
+            $this->toDate = $toDate;
+        } elseif (is_string($toDate)) {
+            $this->toDate = new \DateTime($toDate);
+        } else {
+            throw new \InvalidArgumentException('Invalid type.');
+        }
+    }
+
+    /**
+     * Return time
+     *
+     * @return \DateTime
+     */
+    public function getTime(): \DateTime
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set time
+     *
+     * @param \DateTime|string $time
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    public function setTime($time)
+    {
+        if ($time instanceof \DateTime) {
+            $this->time = $time;
+        } elseif (is_string($time)) {
+            $this->time = new \DateTime($time);
+        } else {
+            throw new \InvalidArgumentException('Invalid type.');
+        }
+    }
+
+    /**
+     * Entity to array
+     *
+     * @return array{type:int,from_date:string,to_date:string|null,time:string}
+     */
+    public function toArray(): array
+    {
+        $data = [];
+        $data['type'] = $this->getType();
+        $data['from_date'] = $this->getFromDate()->format('Y/m/d');
+        $data['to_date'] = is_null($this->getToDate())
+            ? null
+            : $this->getToDate()->format('Y/m/d');
+        $data['time'] = $this->getTime()->format('H:i:s');
+
+        return $data;
+    }
+}
---
+++ tests/Entity/TheaterOpeningHourTest.php
@@ -0,0 +1,368 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Entity;
+
+use Cinemasunshine\ORM\Entity\TheaterOpeningHour;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * TheaterOpeningHour test
+ */
+final class TheaterOpeningHourTest extends TestCase
+{
+    /**
+     * Create target mock
+     *
+     * @return TheaterOpeningHour&\PHPUnit\Framework\MockObject\MockObject
+     */
+    public function createTargetMock()
+    {
+        return $this->createMock(TheaterOpeningHour::class);
+    }
+
+    /**
+     * Create target partial mock
+     *
+     * @param string[] $methods
+     * @return TheaterOpeningHour&\PHPUnit\Framework\MockObject\MockObject
+     */
+    public function createTargetPartialMock(array $methods)
+    {
+        return $this->createPartialMock(TheaterOpeningHour::class, $methods);
+    }
+
+    /**
+     * Create target reflection
+     *
+     * @return \ReflectionClass<TheaterOpeningHour>
+     */
+    public function createTargetReflection()
+    {
+        return new \ReflectionClass(TheaterOpeningHour::class);
+    }
+
+    /**
+     * test create
+     *
+     * @test
+     * @return void
+     */
+    public function testCreate()
+    {
+        $type = 2;
+        $fromDate = '2020/01/01';
+        $toDate = '2020/01/02';
+        $time = '10:00:00';
+        $data = [
+            'type' => $type,
+            'from_date' => $fromDate,
+            'to_date' => $toDate,
+            'time' => $time,
+        ];
+
+        $result = TheaterOpeningHour::create($data);
+        $this->assertInstanceOf(TheaterOpeningHour::class, $result);
+
+        $targetRef = $this->createTargetReflection();
+
+        $typePropertyRef = $targetRef->getProperty('type');
+        $typePropertyRef->setAccessible(true);
+        $this->assertEquals($type, $typePropertyRef->getValue($result));
+
+        $fromDatePropertyRef = $targetRef->getProperty('fromDate');
+        $fromDatePropertyRef->setAccessible(true);
+        $this->assertEquals(
+            $fromDate,
+            $fromDatePropertyRef->getValue($result)->format('Y/m/d')
+        );
+
+        $toDatePropertyRef = $targetRef->getProperty('toDate');
+        $toDatePropertyRef->setAccessible(true);
+        $this->assertEquals(
+            $toDate,
+            $toDatePropertyRef->getValue($result)->format('Y/m/d')
+        );
+
+        $timePropertyRef = $targetRef->getProperty('time');
+        $timePropertyRef->setAccessible(true);
+        $this->assertEquals(
+            $time,
+            $timePropertyRef->getValue($result)->format('H:i:s')
+        );
+    }
+
+    /**
+     * test getType
+     *
+     * @test
+     * @return void
+     */
+    public function testGetType()
+    {
+        $type = 2;
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $typePropertyRef = $targetRef->getProperty('type');
+        $typePropertyRef->setAccessible(true);
+        $typePropertyRef->setValue($targetMock, $type);
+
+        $this->assertEquals($type, $targetMock->getType());
+    }
+
+    /**
+     * test setType
+     *
+     * @test
+     * @return void
+     */
+    public function testSetType()
+    {
+        $type = 2;
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetMock->setType($type);
+
+        $targetRef = $this->createTargetReflection();
+        $typePropertyRef = $targetRef->getProperty('type');
+        $typePropertyRef->setAccessible(true);
+
+        $this->assertEquals($type, $typePropertyRef->getValue($targetMock));
+    }
+
+    /**
+     * test getFromDate
+     *
+     * @test
+     * @return void
+     */
+    public function testGetFromDate()
+    {
+        $fromDate = new \DateTime();
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $fromDatePropertyRef = $targetRef->getProperty('fromDate');
+        $fromDatePropertyRef->setAccessible(true);
+        $fromDatePropertyRef->setValue($targetMock, $fromDate);
+
+        $this->assertEquals($fromDate, $targetMock->getFromDate());
+    }
+
+    /**
+     * test setFromDate
+     *
+     * @test
+     * @return void
+     */
+    public function testSetFromDate()
+    {
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $fromDatePropertyRef = $targetRef->getProperty('fromDate');
+        $fromDatePropertyRef->setAccessible(true);
+
+        $dtObject = new \DateTime();
+        $targetMock->setFromDate($dtObject);
+        $this->assertEquals($dtObject, $fromDatePropertyRef->getValue($targetMock));
+
+        $dtString = '2020-01-01';
+        $targetMock->setFromDate($dtString);
+        $this->assertInstanceOf(
+            \DateTime::class,
+            $fromDatePropertyRef->getValue($targetMock)
+        );
+        $this->assertEquals(
+            $dtString,
+            $fromDatePropertyRef->getValue($targetMock)->format('Y-m-d')
+        );
+    }
+
+    /**
+     * test setFromDate (invalid argument)
+     *
+     * @test
+     * @return void
+     */
+    public function testSetFromDateInvalidArgument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $targetMock = $this->createTargetPartialMock([]);
+
+        /** @phpstan-ignore-next-line */
+        $targetMock->setFromDate(null);
+    }
+
+    /**
+     * test getToDate
+     *
+     * @test
+     * @return void
+     */
+    public function testGetToDate()
+    {
+        $toDate = new \DateTime();
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $toDatePropertyRef = $targetRef->getProperty('toDate');
+        $toDatePropertyRef->setAccessible(true);
+        $toDatePropertyRef->setValue($targetMock, $toDate);
+
+        $this->assertEquals($toDate, $targetMock->getToDate());
+    }
+
+    /**
+     * test setToDate
+     *
+     * @test
+     * @return void
+     */
+    public function testSetToDate()
+    {
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $toDatePropertyRef = $targetRef->getProperty('toDate');
+        $toDatePropertyRef->setAccessible(true);
+
+        $targetMock->setToDate(null);
+        $this->assertEquals(null, $toDatePropertyRef->getValue($targetMock));
+
+        $dtObject = new \DateTime();
+        $targetMock->setToDate($dtObject);
+        $this->assertEquals($dtObject, $toDatePropertyRef->getValue($targetMock));
+
+        $dtString = '2020-01-01';
+        $targetMock->setToDate($dtString);
+        $this->assertInstanceOf(
+            \DateTime::class,
+            $toDatePropertyRef->getValue($targetMock)
+        );
+        $this->assertEquals(
+            $dtString,
+            $toDatePropertyRef->getValue($targetMock)->format('Y-m-d')
+        );
+    }
+
+    /**
+     * test setToDate (invalid argument)
+     *
+     * @test
+     * @return void
+     */
+    public function testSetToDateInvalidArgument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $targetMock = $this->createTargetPartialMock([]);
+
+        /** @phpstan-ignore-next-line */
+        $targetMock->setToDate(123);
+    }
+
+    /**
+     * test getTime
+     *
+     * @test
+     * @return void
+     */
+    public function testGetTime()
+    {
+        $time = new \DateTime('10:00:00');
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $timePropertyRef = $targetRef->getProperty('time');
+        $timePropertyRef->setAccessible(true);
+        $timePropertyRef->setValue($targetMock, $time);
+
+        $this->assertEquals($time, $targetMock->getTime());
+    }
+
+    /**
+     * test setTime
+     *
+     * @test
+     * @return void
+     */
+    public function testSetTime()
+    {
+        $targetMock = $this->createTargetPartialMock([]);
+        $targetRef = $this->createTargetReflection();
+        $timePropertyRef = $targetRef->getProperty('time');
+        $timePropertyRef->setAccessible(true);
+
+        $timeObject = new \DateTime();
+        $targetMock->setTime($timeObject);
+        $this->assertEquals($timeObject, $timePropertyRef->getValue($targetMock));
+
+        $timeString = '10:00:00';
+        $targetMock->setTime($timeString);
+        $this->assertInstanceOf(
+            \DateTime::class,
+            $timePropertyRef->getValue($targetMock)
+        );
+        $this->assertEquals(
+            $timeString,
+            $timePropertyRef->getValue($targetMock)->format('H:i:s')
+        );
+    }
+
+    /**
+     * test setTime (invalid argument)
+     *
+     * @test
+     * @return void
+     */
+    public function testSetTimeInvalidArgument()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $targetMock = $this->createTargetPartialMock([]);
+
+        /** @phpstan-ignore-next-line */
+        $targetMock->setTime(123);
+    }
+
+    /**
+     * test toArray
+     *
+     * @test
+     * @return void
+     */
+    public function testToArray()
+    {
+        $type = 2;
+        $fromDate = new \DateTime('2020/01/01');
+        $toDate = new \DateTime('2020/01/02');
+        $time = new \DateTime('10:00:00');
+
+        $targetMock = $this->createTargetPartialMock([]);
+
+        $targetRef = $this->createTargetReflection();
+        $typePropertyRef = $targetRef->getProperty('type');
+        $typePropertyRef->setAccessible(true);
+        $typePropertyRef->setValue($targetMock, $type);
+
+        $fromDatePropertyRef = $targetRef->getProperty('fromDate');
+        $fromDatePropertyRef->setAccessible(true);
+        $fromDatePropertyRef->setValue($targetMock, $fromDate);
+
+        $toDatePropertyRef = $targetRef->getProperty('toDate');
+        $toDatePropertyRef->setAccessible(true);
+        $toDatePropertyRef->setValue($targetMock, $toDate);
+
+        $timePropertyRef = $targetRef->getProperty('time');
+        $timePropertyRef->setAccessible(true);
+        $timePropertyRef->setValue($targetMock, $time);
+
+        $result = $targetMock->toArray();
+        $this->assertIsArray($result);
+        $this->assertEquals($type, $result['type']);
+        $this->assertEquals($fromDate->format('Y/m/d'), $result['from_date']);
+        $this->assertEquals($toDate->format('Y/m/d'), $result['to_date']);
+        $this->assertEquals($time->format('H:i:s'), $result['time']);
+
+        $toDatePropertyRef->setValue($targetMock, null);
+        $result2 = $targetMock->toArray();
+        $this->assertNull($result2['to_date']);
+    }
+}
